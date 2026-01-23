fc91a65e15ff0d2508f1aa5ad1cddaa242b4723f
Christopher J. Seeger <cjseeger@users.noreply.github.com>
2026-01-23 14:59:38 -0600

    Create results.js

---
+++ docs/assets/js/results.js
@@ -0,0 +1,12 @@
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+  Highcharts.chart('container', {
+    title: {
+      text: 'Example Highcharts Graph'
+    },
+    series: [{
+      data: [1, 3, 2, 4]
+    }]
+  });
+});
+</script>
